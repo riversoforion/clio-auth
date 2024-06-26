9928ff06c903a5dc107204480299e21795632bd0
Eric McIntyre <mac@riversoforion.com>
2024-06-25 22:38:33 -0700

    feat: Unit tests for error module
    Refs: #11

--- src/error.rs
+++ src/error.rs
@@ -3,7 +3,6 @@ use poem::http::StatusCode;
 use std::io;
 use std::net::IpAddr;
 use std::ops::Range;
-use std::string::FromUtf8Error;
 
 use thiserror::Error;
 use tokio::runtime::TryCurrentError;
@@ -40,8 +39,6 @@ pub enum ServerError {
     /// Problem occurred running the server
     #[error("Internal server error")]
     InternalServerError(#[from] io::Error),
-    #[error("Encoding error")]
-    EncodingError(#[from] FromUtf8Error),
     /// No authorization code was received
     #[error("No authorization code received")]
     NoResult,
@@ -51,7 +48,7 @@ impl ResponseError for ServerError {
     fn status(&self) -> StatusCode {
         use ServerError::*;
         match self {
-            EncodingError(_) | NoResult => StatusCode::BAD_REQUEST,
+            NoResult => StatusCode::BAD_REQUEST,
             _ => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
@@ -67,4 +64,70 @@ pub enum AuthError {
     /// without a successful call to `authorize`.
     #[error("No authorization code or PKCE verifier present")]
     InvalidAuthState,
+}
+
+#[cfg(test)]
+mod tests {
+    mod config_error {
+        use crate::ConfigError;
+
+        #[test]
+        fn invalid_server_config() {
+            let error = ConfigError::InvalidServerConfig {
+                expected: "blah".to_owned(),
+                found: "foobar".to_owned(),
+            };
+            assert!(
+                format!("{error}").contains("Invalid server config (expected blah, found foobar)")
+            );
+        }
+
+        #[test]
+        fn cannot_bind_address() {
+            let error = ConfigError::CannotBindAddress {
+                addr: [127, 0, 0, 1].into(),
+                port_range: 5678..6789,
+            };
+            assert!(
+                format!("{error}").contains("Cannot bind to 127.0.0.1 on any port from 5678-6788")
+            )
+        }
+    }
+
+    mod server_error {
+        use crate::ServerError;
+        use poem::error::ResponseError;
+        use poem::http::StatusCode;
+        use std::io;
+        use std::io::ErrorKind;
+
+        #[test]
+        fn no_result_response_error_trait() {
+            let response = ServerError::NoResult.as_response();
+            assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+        }
+
+        #[test]
+        fn internal_server_error_response_error_trait() {
+            let io_error = io::Error::from(ErrorKind::AddrInUse);
+            let response = ServerError::InternalServerError(io_error).as_response();
+            assert_eq!(response.status(), StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    mod auth_error {
+        use crate::AuthError;
+
+        #[test]
+        fn csrf_mismatch() {
+            assert!(format!("{}", AuthError::CsrfMismatch)
+                .contains("Invalid CSRF token (state parameter)"));
+        }
+
+        #[test]
+        fn invalid_auth_state() {
+            assert!(format!("{}", AuthError::InvalidAuthState)
+                .contains("No authorization code or PKCE verifier present"));
+        }
+    }
 }
